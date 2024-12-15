0367caae69d21a87faa9fcc48e52d3861411ffb9
MatthewFro191919 <matthewfroncek4@gmail.com>
2024-12-15 09:14:36 -0500

    Create PosThing.hx

---
+++ source/PosThing.hx
@@ -0,0 +1,21 @@
+package;
+
+class PosThing
+{
+	public var x:Float = 0;
+	public var y:Float = 0;
+	public var z:Float = 0;
+	public var xOffset:Float = 0;
+	public var yOffset:Float = 0;
+	public var zOffset:Float = 0;
+	public var roll:Float = 0;
+	public var pitch:Float = 0;
+	public var yaw:Float = 0;
+	public var rollOffset:Float = 0;
+	public var pitchOffset:Float = 0;
+	public var yawOffset:Float = 0;
+
+	public function new()
+	{
+	}
+}
